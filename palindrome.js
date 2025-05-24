b5d4f2176ac4bfb22f1f1e79a28a9b1b9378fafe
mitzie-ponce <mitzieponce0@gmail.com>
2025-05-25 07:39:58 +0800

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -5,3 +5,7 @@ function isPalindrome(word) {
     let reversed = word.split('').reverse().join('');
     return word === reversed;
 }
+
+console.log(`${word1} is a palindrome: ${isPalindrome(word1)}`);
+console.log(`${word2} is a palindrome: ${isPalindrome(word2)}`);
+
